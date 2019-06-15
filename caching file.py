60348c8d97552893268641db19be38ab19a99d01
Shivam Mittal <32789727+mitalshivam1789@users.noreply.github.com>
2019-06-15 18:47:48 +0530

    Update caching file.py

--- caching file.py
+++ caching file.py
@@ -1,6 +1,6 @@
 import time
 from functools import lru_cache
-@lru_cache(maxsize = 3)
+@lru_cache(maxsize = 3)# it will store 3 files in cache memory
 def s_w(n):
     #some task taking n seconds
     time.sleep(n)
@@ -8,7 +8,7 @@ def s_w(n):
 
 if __name__=='__main__':
     print("now running some work")
-    s_w(3)
+    s_w(3)# it will take time of 3 sec to implement 
     print("done")
-    s_w(3)
+    s_w(3)# it will not kate time as the data is stored in cache memory
     print("called again")
